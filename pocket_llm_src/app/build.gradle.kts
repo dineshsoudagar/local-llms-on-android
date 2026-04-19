b0edbf10dcad109bd43ac3ccd4103e69f8c4e297
dinesh <dinesh.soudagar@gmail.com>
2026-04-19 19:45:00 +0530

    new version 1.4.0

--- pocket_llm_src/app/build.gradle.kts
+++ pocket_llm_src/app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
         minSdk = 24
         targetSdk = 35
         versionCode = 13
-        versionName = "v1.3.0"
+        versionName = "v1.4.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
